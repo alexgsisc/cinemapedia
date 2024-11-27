bf769a470874428aaa7cc0384b0c0a5d3832fa5e
alexisguadarrama <alexgsisc@gmail.com>
2024-11-26 20:00:11 -0600

    configurated theme and go router

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData get theme => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color.fromARGB(255, 26, 140, 211),
+      );
+}
